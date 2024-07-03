ad9c3612871eae2b4b39c849caa53ba0cc110ca6
malgorzatalisak <134603963+malgorzatalisak@users.noreply.github.com>
2024-07-03 20:05:03 +0200

    Create Model.cs

---
+++ VehicleApp/Model.cs
@@ -0,0 +1,7 @@
+public class Model
+{
+    public string Name { get; set; }
+    public int Year { get; set; }
+    public string EngineCapacity { get; set; }
+    public string DriveType { get; set; }
+}
