40eeee83b9bb9bc47dd069720f721ebc17fd74f1
malgorzatalisak <134603963+malgorzatalisak@users.noreply.github.com>
2024-07-03 20:04:24 +0200

    Create SubCategory.cs

---
+++ VehicleApp/SubCategory.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+public class SubCategory
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Country { get; set; }
+    public string Founded { get; set; }
+    public List<Model> Models { get; set; }
+}
